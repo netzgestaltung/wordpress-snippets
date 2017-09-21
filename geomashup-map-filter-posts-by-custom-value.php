71f405372a58c2db8430e357cd4396139485de51
netzgestaltung <t.fellinger@netzgestaltung.at>
2017-09-21 10:42:42 +0200

    added comments

--- geomashup-map-filter-posts-by-custom-value.php
+++ geomashup-map-filter-posts-by-custom-value.php
@@ -3,10 +3,10 @@
   // just dont return anything, when the post is to be hidden
   function yourTheme_geo_mashup_locations_json_filter($json_properties, $queried_object){
     $post_id = $queried_object->object_id;
-    $meta_key = '_hide';
+    $meta_key = '_hide';  // the custom field "_hide" is a checkbox, that is "" when not used or "1" when checked.
     $meta_value = get_post_meta($post_id, $meta_key, true);
     
-    if ( !filter_var($meta_value, FILTER_VALIDATE_BOOLEAN) ) {
+    if ( !filter_var($meta_value, FILTER_VALIDATE_BOOLEAN) ) { // using other types of custom fields. one needs different detection methods here
       return $json_properties;
     }
   }
