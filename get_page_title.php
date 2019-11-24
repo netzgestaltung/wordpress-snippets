de633b16295295bedc1f08bf53d3a429777abcc6
netzgestaltung <t.fellinger@netzgestaltung.at>
2019-11-24 04:53:25 +0100

    Deprecated function, kept for reference

--- get_page_title.php
+++ get_page_title.php
@@ -1,4 +1,15 @@
 <?php 
+/**
+ * Generates a usefull title for all fontend pages, posts, archives, search, 404 and custom post_type/taxonomy pages/archives
+ * @Deprecated
+ * Use wp_get_document_title() instead or add_theme_support('title-tag');
+ * if neccesarry use one of the filters: 
+ * 'pre_get_document_title' -> overwrites the title complete
+ * 'document_title_separator' -> set the separator
+ * 'document_title_parts' -> overwrite parts, array will be imploded and security filtered
+ * @reference: https://developer.wordpress.org/reference/functions/wp_get_document_title/
+ */
+
 // Page title
 function yourTheme_get_page_title($meta = true){
   $title = wp_title('', false); 
