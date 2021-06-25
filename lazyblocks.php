0ce488dbf2982371dc54bb3c8c222dd4856e38b9
netzgestaltung <t.fellinger@netzgestaltung.at>
2021-06-25 11:35:57 +0200

    added jsdoc conform documentation

--- lazyblocks.php
+++ lazyblocks.php
@@ -1,22 +1,32 @@
+/**
+ * Lazy blocks snippets
+ * @
+ */
 
-// remove lazy block wrapper container markup in frontend
-// https://lazyblocks.com/documentation/blocks-code/php-callback/
+/**
+ * remove lazy blocks wrapper container markup in frontend
+ * https://lazyblocks.com/documentation/blocks-code/php-callback/
+ */
 add_filter('lzb/block_render/allow_wrapper', '__return_false');
-
-// lazy block Handlebars helper
-add_action('lzb_handlebars_object', 'myPlugin_lazyblock_handlebars_helper');
   
 /**
- * lazy block Handlebars helper
- * 
- * image sizes helper
- * https://github.com/nk-o/lazy-blocks/issues/68
+ * Additional lazy blocks Handlebars helper
  */
-function myPlugin_lazyblock_handlebars_helper($handlebars){
+function myPlugin_lazyblocks_handlebars_helper($handlebars){
+
+  /**
+   * wp_get_attachment_image Handlebars helper
+   * https://github.com/nk-o/lazy-blocks/issues/68
+   * 
+   * @example
+   * {{{ wp_get_attachment_image control_name 'thumbnail' }}}
+   */
   $handlebars->registerHelper('wp_get_attachment_image', function($image, $size=null){
     if ( isset($image['id']) ) {
       return wp_get_attachment_image($image['id'], $size);
     }	  
   });
 }
-  
+
+// lazy block Handlebars helper
+add_action('lzb_handlebars_object', 'myPlugin_lazyblocks_handlebars_helper');  
